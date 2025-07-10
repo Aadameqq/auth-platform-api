a0e4abde50a6a68fb76445135c15f542f209299b
Aadameqq <bryndza.ad@gmail.com>
2025-07-10 21:09:10 +0200

    refactor: initialize password reset command tests

--- tests/Core.Tests/UseCases/InitializePasswordResetCommandHandlerTests.cs
+++ tests/Core.Tests/UseCases/InitializePasswordResetCommandHandlerTests.cs
@@ -1,97 +1,92 @@
-// using Core.Commands;
-// using Core.Domain;
-// using Core.Exceptions;
-// using Core.Ports;
-// using Moq;
-//
-// namespace Core.Tests.UseCases;
-//
-// public class InitializePasswordResetCommandHandlerTests
-// {
-//     private readonly Mock<AccountsRepository> accountsRepositoryMock = new();
-//     private readonly Mock<PasswordResetCodesRepository> codesRepositoryMock = new();
-//     private readonly Mock<PasswordResetEmailSender> emailSenderMock = new();
-//
-//     private readonly Account existingAccount = new("userName", "email", "password");
-//     private readonly string existingCode = "code";
-//
-//     private readonly InitializePasswordResetCommandHandler commandHandler;
-//
-//     public InitializePasswordResetCommandHandlerTests()
-//     {
-//         commandHandler = new InitializePasswordResetCommandHandler(
-//             accountsRepositoryMock.Object,
-//             codesRepositoryMock.Object,
-//             emailSenderMock.Object
-//         );
-//
-//         accountsRepositoryMock
-//             .Setup(x => x.FindByEmail(existingAccount.Email))
-//             .ReturnsAsync(existingAccount);
-//
-//         codesRepositoryMock.Setup(x => x.Create(It.IsAny<Account>())).ReturnsAsync(existingCode);
-//     }
-//
-//     [Fact]
-//     public async Task WhenAccountWithGivenEmailDoesNotExist_ShouldFail()
-//     {
-//         var result = await commandHandler.Execute("invalid-email");
-//
-//         Assert.True(result.IsFailure);
-//         Assert.IsType<NoSuch<Account>>(result.Exception);
-//         AssertNoChanges();
-//     }
-//
-//     [Fact]
-//     public async Task WhenAccountWithGivenEmailExists_ShouldSucceed()
-//     {
-//         var result = await commandHandler.Execute(existingAccount.Email);
-//
-//         Assert.True(result.IsSuccess);
-//     }
-//
-//     [Fact]
-//     public async Task WhenAccountWithGivenEmailExists_ShouldCreateCode()
-//     {
-//         Account? actualAccount = null;
-//         codesRepositoryMock
-//             .Setup(x => x.Create(It.IsAny<Account>()))
-//             .Callback((Account a) => actualAccount = a);
-//
-//         await commandHandler.Execute(existingAccount.Email);
-//
-//         Assert.NotNull(actualAccount);
-//         Assert.Equivalent(existingAccount, actualAccount);
-//     }
-//
-//     [Fact]
-//     public async Task WhenAccountWithGivenEmailExists_ShouldSendEmail()
-//     {
-//         Account? actualAccount = null;
-//         var actualCode = string.Empty;
-//
-//         emailSenderMock
-//             .Setup(x => x.Send(It.IsAny<Account>(), It.IsAny<string>()))
-//             .Callback(
-//                 (Account a, string c) =>
-//                 {
-//                     actualAccount = a;
-//                     actualCode = c;
-//                 }
-//             );
-//
-//         await commandHandler.Execute(existingAccount.Email);
-//
-//         Assert.Equivalent(existingAccount, actualAccount);
-//         Assert.Same(existingCode, actualCode);
-//     }
-//
-//     private void AssertNoChanges()
-//     {
-//         accountsRepositoryMock.Verify(x => x.Flush(), Times.Never);
-//         codesRepositoryMock.Verify(x => x.Create(It.IsAny<Account>()), Times.Never);
-//         emailSenderMock.Verify(x => x.Send(It.IsAny<Account>(), It.IsAny<string>()), Times.Never);
-//     }
-// }
+using Core.Commands;
+using Core.Commands.Commands;
+using Core.Domain;
+using Core.Exceptions;
+using Core.Ports;
+using NSubstitute;
 
+namespace Core.Tests.UseCases;
 
+public class InitializePasswordResetCommandHandlerTests
+{
+    private readonly AccountsRepository accountsRepositoryMock =
+        Substitute.For<AccountsRepository>();
+
+    private readonly PasswordResetCodesRepository codesRepositoryMock =
+        Substitute.For<PasswordResetCodesRepository>();
+
+    private readonly InitializePasswordResetCommandHandler commandHandler;
+
+    private readonly PasswordResetEmailSender emailSenderMock =
+        Substitute.For<PasswordResetEmailSender>();
+
+    private readonly Account existingAccount = new("userName", "email", "password");
+    private readonly string existingCode = "code";
+    private readonly UnitOfWork uowMock = Substitute.For<UnitOfWork>();
+
+    public InitializePasswordResetCommandHandlerTests()
+    {
+        commandHandler = new InitializePasswordResetCommandHandler(
+            uowMock,
+            codesRepositoryMock,
+            emailSenderMock
+        );
+
+        uowMock.GetAccountsRepository().Returns(accountsRepositoryMock);
+
+        accountsRepositoryMock.FindByEmail(existingAccount.Email).Returns(existingAccount);
+
+        codesRepositoryMock.Create(Arg.Any<Account>()).Returns(existingCode);
+    }
+
+    [Fact]
+    public async Task WhenAccountWithGivenEmailDoesNotExist_ShouldFail()
+    {
+        var result = await RunCommand("invalid-email");
+
+        Assert.True(result.IsFailure);
+        Assert.IsType<NoSuch<Account>>(result.Exception);
+        AssertNoChanges();
+    }
+
+    [Fact]
+    public async Task WhenAccountWithGivenEmailExists_ShouldSucceed()
+    {
+        var result = await RunCommand(existingAccount.Email);
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task WhenAccountWithGivenEmailExists_ShouldCreateCode()
+    {
+        await RunCommand(existingAccount.Email);
+
+        await codesRepositoryMock
+            .Received()
+            .Create(Arg.Is<Account>(a => a.Id == existingAccount.Id));
+    }
+
+    [Fact]
+    public async Task WhenAccountWithGivenEmailExists_ShouldSendEmail()
+    {
+        await RunCommand(existingAccount.Email);
+
+        await emailSenderMock
+            .Received()
+            .Send(Arg.Is<Account>(a => a.Id == existingAccount.Id), Arg.Is(existingCode));
+    }
+
+    private void AssertNoChanges()
+    {
+        uowMock.DidNotReceive().Flush();
+        codesRepositoryMock.DidNotReceive().Create(Arg.Any<Account>());
+        emailSenderMock.DidNotReceive().Send(Arg.Any<Account>(), Arg.Any<string>());
+    }
+
+    private Task<Result> RunCommand(string email)
+    {
+        var cmd = new InitializePasswordResetCommand(email);
+        return commandHandler.Handle(cmd, CancellationToken.None);
+    }
+}
