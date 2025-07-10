7c37c2f23271640466e5661826e1c3fb9e79d9e9
Aadameqq <bryndza.ad@gmail.com>
2025-07-10 21:49:35 +0200

    refactor: unassign role command tests

--- tests/Core.Tests/UseCases/UnassignRoleCommandHandlerTests.cs
+++ tests/Core.Tests/UseCases/UnassignRoleCommandHandlerTests.cs
@@ -1,73 +1,77 @@
-// using Core.Commands;
-// using Core.Domain;
-// using Core.Exceptions;
-// using Core.Ports;
-// using Moq;
-//
-// namespace Core.Tests.UseCases;
-//
-// public class UnassignRoleCommandHandlerTests
-// {
-//     private readonly Mock<AccountsRepository> accountsRepositoryMock = new();
-//
-//     private readonly Account existingAccount = new("userName", "email", "password");
-//
-//     private readonly UnassignRoleCommandHandler commandHandler;
-//
-//     public UnassignRoleCommandHandlerTests()
-//     {
-//         commandHandler = new UnassignRoleCommandHandler(accountsRepositoryMock.Object);
-//
-//         accountsRepositoryMock
-//             .Setup(r => r.FindById(existingAccount.Id))
-//             .ReturnsAsync(existingAccount);
-//     }
-//
-//     [Fact]
-//     public async Task ShouldFail_WhenAccountWithGivenIdDoesNotExist()
-//     {
-//         var result = await commandHandler.Execute(Guid.NewGuid(), Guid.NewGuid());
-//
-//         Assert.True(result.IsFailure);
-//         Assert.IsType<NoSuch<Account>>(result.Exception);
-//         AssertNoChanges();
-//     }
-//
-//     [Fact]
-//     public async Task ShouldFail_WhenAccountAttemptsToSelfUnassignRole()
-//     {
-//         var result = await commandHandler.Execute(existingAccount.Id, existingAccount.Id);
-//
-//         Assert.True(result.IsFailure);
-//         Assert.IsType<CannotManageOwn<Role>>(result.Exception);
-//         AssertNoChanges();
-//     }
-//
-//     [Fact]
-//     public async Task ShouldSucceed_WhenAccountWithGivenIdExistsAndIsNotIssuer()
-//     {
-//         var result = await commandHandler.Execute(Guid.NewGuid(), existingAccount.Id);
-//
-//         Assert.True(result.IsSuccess);
-//     }
-//
-//     [Fact]
-//     public async Task ShouldPersistCorrectAccount_WhenAccountWithGivenIdExistsAndIsNotIssuer()
-//     {
-//         Account actualAccount = null!;
-//
-//         accountsRepositoryMock
-//             .Setup(r => r.UpdateAndFlush(It.IsAny<Account>()))
-//             .Callback((Account a) => actualAccount = a);
-//
-//         await commandHandler.Execute(Guid.NewGuid(), existingAccount.Id);
-//
-//         Assert.NotNull(actualAccount);
-//         Assert.Equal(existingAccount.Id, actualAccount.Id);
-//     }
-//
-//     private void AssertNoChanges()
-//     {
-//         accountsRepositoryMock.Verify(r => r.UpdateAndFlush(It.IsAny<Account>()), Times.Never);
-//     }
-// }
+using Core.Commands;
+using Core.Commands.Commands;
+using Core.Domain;
+using Core.Exceptions;
+using Core.Ports;
+using NSubstitute;
+
+namespace Core.Tests.UseCases;
+
+public class UnassignRoleCommandHandlerTests
+{
+    private readonly AccountsRepository accountsRepositoryMock =
+        Substitute.For<AccountsRepository>();
+
+    private readonly UnassignRoleCommandHandler commandHandler;
+
+    private readonly Account existingAccount = new("userName", "email", "password");
+    private readonly UnitOfWork uowMock = Substitute.For<UnitOfWork>();
+
+    public UnassignRoleCommandHandlerTests()
+    {
+        commandHandler = new UnassignRoleCommandHandler(uowMock);
+
+        uowMock.GetAccountsRepository().Returns(accountsRepositoryMock);
+
+        accountsRepositoryMock.FindById(existingAccount.Id).Returns(existingAccount);
+    }
+
+    [Fact]
+    public async Task ShouldFail_WhenAccountWithGivenIdDoesNotExist()
+    {
+        var result = await RunCommand(Guid.NewGuid(), Guid.NewGuid());
+
+        Assert.True(result.IsFailure);
+        Assert.IsType<NoSuch<Account>>(result.Exception);
+        AssertNoChanges();
+    }
+
+    [Fact]
+    public async Task ShouldFail_WhenAccountAttemptsToSelfUnassignRole()
+    {
+        var result = await RunCommand(existingAccount.Id, existingAccount.Id);
+
+        Assert.True(result.IsFailure);
+        Assert.IsType<CannotManageOwn<Role>>(result.Exception);
+        AssertNoChanges();
+    }
+
+    [Fact]
+    public async Task ShouldSucceed_WhenAccountWithGivenIdExistsAndIsNotIssuer()
+    {
+        var result = await RunCommand(Guid.NewGuid(), existingAccount.Id);
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task ShouldPersistCorrectAccount_WhenAccountWithGivenIdExistsAndIsNotIssuer()
+    {
+        await RunCommand(Guid.NewGuid(), existingAccount.Id);
+
+        await accountsRepositoryMock
+            .Received()
+            .Update(Arg.Is<Account>(a => a.Id == existingAccount.Id));
+    }
+
+    private Task<Result> RunCommand(Guid issuerId, Guid accountId)
+    {
+        var cmd = new UnassignRoleCommand(issuerId, accountId);
+        return commandHandler.Handle(cmd, CancellationToken.None);
+    }
+
+    private void AssertNoChanges()
+    {
+        uowMock.DidNotReceive().Flush();
+    }
+}
