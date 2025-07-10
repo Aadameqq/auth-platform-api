8fed985104f3998d022e10820124499addecfed4
Aadameqq <bryndza.ad@gmail.com>
2025-07-10 21:56:42 +0200

    refactor: reset password command tests

--- tests/Core.Tests/UseCases/ResetPasswordCommandHandlerTests.cs
+++ tests/Core.Tests/UseCases/ResetPasswordCommandHandlerTests.cs
@@ -1,97 +1,99 @@
-// using Core.Commands;
-// using Core.Domain;
-// using Core.Exceptions;
-// using Core.Ports;
-// using Moq;
-//
-// namespace Core.Tests.UseCases;
-//
-// public class ResetPasswordCommandHandlerTests
-// {
-//     private readonly Mock<AccountsRepository> accountsRepositoryMock = new();
-//     private readonly Mock<PasswordResetCodesRepository> codesRepositoryMock = new();
-//
-//     private readonly Account existingAccount = new("userName", "email", "password");
-//     private readonly string existingCode = "code";
-//
-//     private readonly Mock<PasswordHasher> passwordHasherMock = new();
-//
-//     private readonly string testPassword = "new-password";
-//     private readonly string testPasswordHashed = "new-password-hash";
-//
-//     private readonly ResetPasswordCommandHandler commandHandler;
-//
-//     public ResetPasswordCommandHandlerTests()
-//     {
-//         commandHandler = new ResetPasswordCommandHandler(
-//             passwordHasherMock.Object,
-//             accountsRepositoryMock.Object,
-//             codesRepositoryMock.Object
-//         );
-//
-//         codesRepositoryMock
-//             .Setup(x => x.GetAccountIdAndRevokeCode(existingCode))
-//             .ReturnsAsync(existingAccount.Id);
-//
-//         accountsRepositoryMock
-//             .Setup(x => x.FindById(existingAccount.Id))
-//             .ReturnsAsync(existingAccount);
-//
-//         passwordHasherMock.Setup(x => x.HashPassword(testPassword)).Returns(testPasswordHashed);
-//     }
-//
-//     [Fact]
-//     public async Task WhenGivenCodeIsInvalid_ShouldFail()
-//     {
-//         var result = await commandHandler.Execute("invalid-code", "some-password");
-//
-//         Assert.True(result.IsFailure);
-//         Assert.IsType<NoSuch>(result.Exception);
-//         AssertNoChanges();
-//     }
-//
-//     [Fact]
-//     public async Task WhenGivenCodeIsValidButAccountAssignedToItDoesNotExist_ShouldFail()
-//     {
-//         accountsRepositoryMock
-//             .Setup(x => x.FindById(It.IsAny<Guid>()))
-//             .ReturnsAsync(null as Account);
-//
-//         var result = await commandHandler.Execute(existingCode, "some-password");
-//
-//         Assert.True(result.IsFailure);
-//         Assert.IsType<NoSuch>(result.Exception);
-//         AssertNoChanges();
-//     }
-//
-//     [Fact]
-//     public async Task WhenGivenCodeIsValid_ShouldSucceed()
-//     {
-//         var result = await commandHandler.Execute(existingCode, "new-password");
-//
-//         Assert.True(result.IsSuccess);
-//     }
-//
-//     [Fact]
-//     public async Task WhenGivenCodeIsValid_ShouldChangePasswordAndPersist()
-//     {
-//         Account actualAccount = null!;
-//
-//         accountsRepositoryMock
-//             .Setup(x => x.UpdateAndFlush(It.IsAny<Account>()))
-//             .Callback((Account a) => actualAccount = a);
-//
-//         await commandHandler.Execute(existingCode, testPassword);
-//
-//         Assert.Equal(existingAccount.Id, actualAccount.Id);
-//         Assert.Equal(testPasswordHashed, actualAccount.Password);
-//     }
-//
-//     private void AssertNoChanges()
-//     {
-//         accountsRepositoryMock.Verify(x => x.UpdateAndFlush(It.IsAny<Account>()), Times.Never);
-//         codesRepositoryMock.Verify(x => x.Create(It.IsAny<Account>()), Times.Never);
-//     }
-// }
+using Core.Commands;
+using Core.Commands.Commands;
+using Core.Domain;
+using Core.Exceptions;
+using Core.Ports;
+using NSubstitute;
 
+namespace Core.Tests.UseCases;
 
+public class ResetPasswordCommandHandlerTests
+{
+    private readonly AccountsRepository accountsRepositoryMock =
+        Substitute.For<AccountsRepository>();
+
+    private readonly PasswordResetCodesRepository codesRepositoryMock =
+        Substitute.For<PasswordResetCodesRepository>();
+
+    private readonly ResetPasswordCommandHandler commandHandler;
+
+    private readonly Account existingAccount = new("userName", "email", "password");
+    private readonly string existingCode = "code";
+
+    private readonly PasswordHasher passwordHasherMock = Substitute.For<PasswordHasher>();
+
+    private readonly string testPassword = "new-password";
+    private readonly string testPasswordHashed = "new-password-hash";
+    private readonly UnitOfWork uowMock = Substitute.For<UnitOfWork>();
+
+    public ResetPasswordCommandHandlerTests()
+    {
+        commandHandler = new ResetPasswordCommandHandler(
+            passwordHasherMock,
+            uowMock,
+            codesRepositoryMock
+        );
+
+        codesRepositoryMock.GetAccountIdAndRevokeCode(existingCode).Returns(existingAccount.Id);
+
+        accountsRepositoryMock.FindById(existingAccount.Id).Returns(existingAccount);
+
+        passwordHasherMock.HashPassword(testPassword).Returns(testPasswordHashed);
+
+        uowMock.GetAccountsRepository().Returns(accountsRepositoryMock);
+    }
+
+    [Fact]
+    public async Task WhenGivenCodeIsInvalid_ShouldFail()
+    {
+        var result = await RunCommand("invalid-code", "some-password");
+
+        Assert.True(result.IsFailure);
+        Assert.IsType<NoSuch>(result.Exception);
+        AssertNoChanges();
+    }
+
+    [Fact]
+    public async Task WhenGivenCodeIsValidButAccountAssignedToItDoesNotExist_ShouldFail()
+    {
+        accountsRepositoryMock.FindById(Arg.Any<Guid>()).Returns(null as Account);
+
+        var result = await RunCommand(existingCode, "some-password");
+
+        Assert.True(result.IsFailure);
+        Assert.IsType<NoSuch>(result.Exception);
+        AssertNoChanges();
+    }
+
+    [Fact]
+    public async Task WhenGivenCodeIsValid_ShouldSucceed()
+    {
+        var result = await RunCommand(existingCode, "new-password");
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task WhenGivenCodeIsValid_ShouldChangePasswordAndPersist()
+    {
+        await RunCommand(existingCode, testPassword);
+
+        await accountsRepositoryMock
+            .Received()
+            .Update(
+                Arg.Is<Account>(a => a.Id == existingAccount.Id && a.Password == testPasswordHashed)
+            );
+    }
+
+    private void AssertNoChanges()
+    {
+        uowMock.DidNotReceive().Flush();
+        codesRepositoryMock.DidNotReceive().Create(Arg.Any<Account>());
+    }
+
+    private Task<Result> RunCommand(string resetCode, string newPassword)
+    {
+        var cmd = new ResetPasswordCommand(resetCode, newPassword);
+        return commandHandler.Handle(cmd, CancellationToken.None);
+    }
+}
